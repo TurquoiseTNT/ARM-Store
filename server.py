dda0cf3b107436dcdcd4380b3125fd9d8ed716cf
TurquoiseTNT <sebastianneill2011@gmail.com>
2023-01-06 21:57:07 +0000

    Cool UI and working system

--- server.py
+++ server.py
@@ -6,9 +6,9 @@ app = Flask(__name__)
 @app.route("/installapp/<appname>")
 def installapp(appname):
     os.system("curl https://raw.githubusercontent.com/TurquoiseTNT/ARM-Store-Installers/main/" + appname + "/install || bash")
-    return "APP INSTALLED"
+    return "APP INSTALLED <a href='tnt.is-a.dev/ARM-Store'><button>Back to App List</button></a>"
     
 @app.route("/removeapp/<appname>")
 def installapp(appname):
     os.system("curl https://raw.githubusercontent.com/TurquoiseTNT/ARM-Store-Installers/main/" + appname + "/uninstall || bash")
-    return "APP UNINSTALLED"
+    return "APP REMOVED <a href='tnt.is-a.dev/ARM-Store'><button>Back to App List</button></a>"
